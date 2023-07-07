774c150867b4ffd079224baa693678dcdc5fa7cc
pamela <pamelamorsentti@gmail.com>
2023-07-07 01:33:00 -0300

    con models y algunos datos de ejemplos cargados

--- vista/admin.py
+++ vista/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from vista.models import Categoria
+from vista.models import Producto
 
+class ProductoAdmin(admin.ModelAdmin):
+    fields =['categoria','fecha_publicacion', 'producto', 'imagen']
+    
+admin.site.register(Producto, ProductoAdmin)
+    
 # Register your models here.
+admin.site.register(Categoria)
+#admin.site.register(Producto)
