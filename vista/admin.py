5bcc0fca5c2c74ac79b7d44de3ab0bbf6457b389
pamela <pamelamorsentti@gmail.com>
2023-07-07 01:48:08 -0300

    cambio en archivo admin.py

--- vista/admin.py
+++ vista/admin.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from vista.models import Categoria
 from vista.models import Producto
 
+def publicar(modeladmin, request, queryset):
+    queryset.update(estado='Publicado')
+
+
 class ProductoAdmin(admin.ModelAdmin):
     fields =['categoria','fecha_publicacion', 'producto', 'imagen']
-    
-admin.site.register(Producto, ProductoAdmin)
+    #list_display = ['producto', 'fecha_publicacion', 'imagen', 'tipo_de_producto',]
+    ordering = ['-fecha_publicacion']
+    list_filter = ('producto', 'fecha_publicacion',)
+    actions = [publicar]
     
 # Register your models here.
 admin.site.register(Categoria)
 #admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
